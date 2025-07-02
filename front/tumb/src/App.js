2df318e0ec04b4478a9c8755357e0d71ea156f36
leeseungjun <dltmdwns0731@gmail.com>
2025-07-02 20:30:07 +0900

    v3

--- front/tumb/src/App.js
+++ front/tumb/src/App.js
@@ -1,15 +1,17 @@
-import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate, BrowserRouter } from 'react-router-dom';
 import Log_in from './Pages/Log_in';
-import Sign_up from './Pages/Sign_up';
+import Sign_up from './Pages/sign_up';
 
 const App = () => {
 
     return(
         <div>
+          <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Log_in/>}/>
                 <Route path='/sign_up' element={<Sign_up/>}/>
             </Routes>
+          </BrowserRouter>  
         </div>
 
     );
