94ab45271afb2727dafdf5032e855b600b4cc771
khw614 <heeone2001@naver.com>
2025-07-04 20:37:59 +0900

    로그인시 사용자 정보 불러오기

---
+++ back/src/main/java/likelion/Sugang/DTO/UserLoginDTO.java
@@ -0,0 +1,13 @@
+package likelion.Sugang.DTO;
+
+import lombok.*;
+
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginDTO {
+    private Integer userId;
+    private String password;
+}
